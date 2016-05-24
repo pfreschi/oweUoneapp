c6776c1775d5053cbcfa639cf5348a526bcc0cce
Peter Freschi <pfreschi@uw.edu>
2016-05-23 22:45:20 -0700

    prepare for merge

--- oweUone/LoginViewController.swift
+++ oweUone/LoginViewController.swift
@@ -18,11 +18,15 @@ import FBSDKLoginKit
 
 class LoginViewController: UIViewController {
     
-    
+    var userPhoneNum : String = ""
     let rootRef = FIRDatabase.database().reference()
     var imageView : UIImageView!
+    var newUser : [String: String] = [ "provider" : "",
+                                       "Name": "",
+                                       "Email": "",
+                                       "Phone": ""
+    ]
     
-    var enteredPhoneNumber : String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,39 +52,80 @@ class LoginViewController: UIViewController {
                 
             } else {
                 
-                //self.getProfilePic()
+                self.getProfilePic()
                 
                 // use FB Access token to switch for a firebase access token
                 let credential = FIRFacebookAuthProvider.credentialWithAccessToken(FBSDKAccessToken.currentAccessToken().tokenString)
                 self.firebaseLogin(credential)
                 
-                //when user logged in, automatically take user to the favors feed view
-                let nextView = (self.storyboard?.instantiateViewControllerWithIdentifier("tabBar"))! as UIViewController
-                self.presentViewController(nextView, animated: true, completion: nil)
             }
         })
     }
     
-    /*
-     func getProfilePic() {
-     if FBSDKAccessToken.currentAccessToken() != nil {
-     let graphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, email"])
-     graphRequest.startWithCompletionHandler({ (connection, user, requestError) -> Void in
-     
-     // get user FB profile picture
-     let FBid = user.valueForKey("id") as? String
-     let url = NSURL(string: "https://graph.facebook.com/\(FBid!)/picture?type=large&return_ssl_resources=1")
-     self.imageView.image = UIImage(data: NSData(contentsOfURL: url!)!)
-     
-     })
-     }
-     }
-     */
+    func showFeed() {
+        //when user logged in, automatically take user to the favors feed view
+        let nextView = (self.storyboard?.instantiateViewControllerWithIdentifier("tabBar"))! as UIViewController
+        self.presentViewController(nextView, animated: true, completion: nil)
+        
+    }
+    
+    func getPhoneNumAlert(userUid: String) {
+        let alertController = UIAlertController.init(title: "Login Successful", message: "Please enter your phone number", preferredStyle: .Alert)
+        
+        let updateAction = UIAlertAction(title:"Submit", style:.Default, handler: {
+            alert -> Void in
+            
+            self.userPhoneNum = alertController.textFields![0].text!
+            self.newUser["Phone"] = self.userPhoneNum
+            
+            self.addNewUser(userUid)
+            self.showFeed()
+            self.setNeedsFocusUpdate()
+            
+            
+        })
+        
+        alertController.addAction(updateAction)
+        
+        alertController.addTextFieldWithConfigurationHandler { (textField : UITextField!) -> Void in
+            textField.placeholder = "000-000-0000"
+        }
+        
+        alertController.view.setNeedsLayout()
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    // add new user into firebase
+    func addNewUser(userUid: String) {
+        self.rootRef.child("users").child(userUid).setValue(self.newUser)
+    }
+    
+    // get user's info with facebook token
+    func getProfilePic() {
+        if FBSDKAccessToken.currentAccessToken() != nil {
+            let graphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, email"])
+            graphRequest.startWithCompletionHandler({ (connection, user, requestError) -> Void in
+                
+                // get user FB profile picture
+                let FBid = user.valueForKey("id") as? String
+                
+                // store facebook ID for future access
+                NSUserDefaults.standardUserDefaults().setValue(FBid, forKey: "FBid")
+                
+                //let url = NSURL(string: "https://graph.facebook.com/\(FBid!)/picture?type=large&return_ssl_resources=1")
+                // self.imageView.image = UIImage(data: NSData(contentsOfURL: url!)!)
+                
+            })
+        }
+    }
     
     func firebaseLogin(credential: FIRAuthCredential) {
         
         if (FIRAuth.auth()?.currentUser?.linkWithCredential) != nil {
             print("Current user has been linked with a firebase credential.")
+            
+            //when user logged in, automatically take user to the favors feed view
+            showFeed()
             
         } else {
             
@@ -92,49 +137,41 @@ class LoginViewController: UIViewController {
                     return
                 } else {
                     
-                    //force user to give phone number
-                    print("phone number is \(self.enteredPhoneNumber)")
-                    if (self.enteredPhoneNumber == "") {
-                        dispatch_async(dispatch_get_main_queue()){
-                            self.performSegueWithIdentifier("requestPhone", sender: self)
-                        }
-                    }
-                    
                     
                     // add the new user to Firebase database
                     for profile in user!.providerData {
-                        var newUser : [String: String] = [
-                            "provider": profile.providerID,
-                            "Name": profile.displayName!,
-                            "Email": profile.email!,
-                            "School" : "University of Washington"
-                            //school should be more flexible in the future, with FB's approval for access to this data
-                        ]
+                        let userUid = profile.uid
                         
-                    
-                        newUser["Phone"] = self.enteredPhoneNumber
+                        self.newUser["provider"] = profile.providerID
+                        self.newUser["Name"] = profile.displayName!
+                        self.newUser["Email"] = profile.email!
+                        // "School": "University of Washington"
                         
-                        //store
-                        self.rootRef.child("users").child(profile.uid).setValue(newUser)
+                        
+                        // prompts a alert controller to ask for user phone number
+                        self.getPhoneNumAlert(userUid)
+                        
+                        
+                        
                         
                         
                         //add school to database? implement later
                         /*
-                        self.rootRef.observeEventType(.Value, withBlock: { (snapshot) in
-                            print(snapshot.value)
-
-                            if let schools = snapshot.childSnapshotForPath("schools") as FIRDataSnapshot! {
-                                        if let schoolDict = schools.value as? Dictionary<String, AnyObject> {
-                                        print(schoolDict)
-                                        //let key = snap.key
-                                        //let favor = Favor(key: key, dictionary: favorDict)
-                                        
-                                        //self.favorsList.insert(favor, atIndex: 0)
-                                    }
-                                }
-                            })
-                        */
-
+                         self.rootRef.observeEventType(.Value, withBlock: { (snapshot) in
+                         print(snapshot.value)
+                         
+                         if let schools = snapshot.childSnapshotForPath("schools") as FIRDataSnapshot! {
+                         if let schoolDict = schools.value as? Dictionary<String, AnyObject> {
+                         print(schoolDict)
+                         //let key = snap.key
+                         //let favor = Favor(key: key, dictionary: favorDict)
+                         
+                         //self.favorsList.insert(favor, atIndex: 0)
+                         }
+                         }
+                         })
+                         */
+                        
                     }
                 }
             }
@@ -142,4 +179,3 @@ class LoginViewController: UIViewController {
     }
     
 }
-

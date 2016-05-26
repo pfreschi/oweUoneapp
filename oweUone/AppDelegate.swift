11a4af44ea4b65364769e95290505398e61e84d1
Xiaowen Feng <xfeng1@uw.edu>
2016-05-25 21:32:59 -0700

    Added remember logged in

--- oweUone/AppDelegate.swift
+++ oweUone/AppDelegate.swift
@@ -19,6 +19,7 @@ import FirebaseDatabase
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var storyboard: UIStoryboard?
 
 
     override init() {
@@ -30,6 +31,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+      self.storyboard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+        if  NSUserDefaults.standardUserDefaults().valueForKey("uid") != nil {
+            self.window?.rootViewController = self.storyboard?.instantiateViewControllerWithIdentifier("tabBar")
+        } else {
+            self.window?.rootViewController = self.storyboard?.instantiateViewControllerWithIdentifier("Login")
+        }
+       
+        
         return FBSDKApplicationDelegate.sharedInstance()
             .application(application, didFinishLaunchingWithOptions: launchOptions)
     }
--- oweUone/PostFavorViewController.swift
+++ oweUone/PostFavorViewController.swift
@@ -10,38 +10,93 @@ import UIKit
 import Firebase
 
 class PostFavorViewController: UIViewController {
+    
+    let userRef = FirebaseProxy.firebaseProxy.userRef
+    var uid : String = ""
     @IBOutlet weak var favorTitle: UITextField!
     
     @IBOutlet weak var favorDescription: UITextField!
 
     @IBOutlet weak var tokenAmount: UITextField!
     
+    @IBOutlet weak var warningText: UILabel!
+    
     @IBAction func postFavor(sender: AnyObject) {
         
-        if let user = FIRAuth.auth()?.currentUser {
-            for profile in user.providerData {
-                let uid = profile.uid;  // Provider-specific UID
-                if (favorTitle.text != nil && favorDescription.text != nil && tokenAmount.text != nil){
-                    FirebaseProxy.firebaseProxy.saveFavor(favorTitle.text!, descr: favorDescription.text!, tokenAmount: Int(tokenAmount.text!)!, creator: uid)
-                }
-                
-
-            }
+        let title = favorTitle.text
+        let descr = favorDescription.text
+        let token = tokenAmount.text
+        
+        
+        if (title!.isEmpty || descr!.isEmpty || token!.isEmpty) {
+            
+           // validationAlert("Please fill out all the information!")
+            warningText.text = "Please fill out all the information!"
+        
+        } else if !title!.isEmpty && !descr!.isEmpty && !checkToken(uid, postingTokens: Int(token!)!) {
+            
+            //validationAlert("Sorry, you don't have enough money.")
+            warningText.text = "Sorry, you don't have enough money."
+ 
             
         } else {
-            // No user is signed in.
-        }
+            
+            if let user = FIRAuth.auth()?.currentUser {
+                for profile in user.providerData {
+                    uid = profile.uid;  // Provider-specific UID
+                    
+                    FirebaseProxy.firebaseProxy.saveFavor(title!, descr: descr!, tokenAmount: Int(token!)!, creator: uid)
+                                        
+                }
+                
+            }
+            performSegueWithIdentifier("backToFeed", sender: self)
 
-        
-        
-        
+        }
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(FIRAuth.auth()?.currentUser?.displayName)
-        print(FIRAuth.auth()?.currentUser?.uid)
+      //  print(FIRAuth.auth()?.currentUser?.displayName)
+      //  print(FIRAuth.auth()?.currentUser?.uid)
+    }
+    
+    func checkToken(uid: String, postingTokens: Int) -> Bool {
+        var hasEnoughToken = true
+        
+        self.userRef.child(uid).observeSingleEventOfType(.Value, withBlock: { (snapshot) in
+            let tokens = snapshot.value!["Tokens"] as! Int
+            
+            if tokens < postingTokens {
+                hasEnoughToken = false
             }
+        })
+        
+        return hasEnoughToken
+        
+    }
+    
+   
+    
+    /*
+    func validationAlert (message: String) {
+        let validationAlert = UIAlertController(title:"Error", message: message, preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        
+        validationAlert.addAction(okAction)
+        
+        let TopVC = UIApplication.sharedApplication().keyWindow?.rootViewController
+        //dispatch_async(dispatch_get_main_queue(), {
+            TopVC!.presentViewController(validationAlert, animated: true, completion: nil)
+            
+       // })
+        
+        //return
+        
+        
+        
+    }
+ */
     
 }
